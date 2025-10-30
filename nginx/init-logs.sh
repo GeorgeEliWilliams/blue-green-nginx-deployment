56eb6c2287a91302fef005433fa4100d788dbb8c
George Williams <williamsgeorge950@gmail.com>
2025-10-30 16:20:23 +0000

    Add a small init script to recreate the log file properly

---
+++ nginx/init-logs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Recreate actual log files instead of using Docker symlinks
+mkdir -p /var/log/nginx
+
+# Remove symlinks to stdout/stderr (the default behavior)
+rm -f /var/log/nginx/access.log /var/log/nginx/error.log
+
+# Create real log files
+touch /var/log/nginx/access.log /var/log/nginx/error.log
+
+# Give permissions
+chmod 666 /var/log/nginx/*.log
+
+# Start nginx normally
+nginx -g 'daemon off;'
